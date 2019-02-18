7bc98b76871832f92c3efa6004a55fdb04fb90a6
root <root@instance-5.softappsit.biz>
2019-02-18 05:40:31 +0000

    first commit

---
+++ odoo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+source /pd_build/buildconfig
+
+header "Installing Odoo Runint Script... "
+
+## Install Odoo runit service.
+run mkdir /etc/service/odoo
+run cp /pd_build/runit/odoo /etc/service/odoo/run
+run touch /etc/service/odoo/down
+header "Installing Odoo..."
+
+## Install Odoo.
+run sudo -i -u odoo
+curl -Lf -o /opt/odoo/odoo.tar.gz https://github.com/trabacus-softapps/odoo12/archive/master.tar.gz
+run mkdir -p /opt/odoo/{bin,etc,sources/odoo/addons,additional_addons,data,scrape} && \
+        cd /opt/odoo/sources/odoo && \
+        tar -xvf /opt/odoo/odoo.tar.gz --strip 1 && \
+	cp -r -p /opt/odoo/sources/odoo/setup/odoo /opt/odoo/sources/odoo/openerp-server && \
+        rm /opt/odoo/odoo.tar.gz
+run mkdir -p /opt/odoo/var/{run,log,egg-cache,ftp}
+
+run cp /pd_build/sources/odoo.conf /opt/odoo/etc/odoo.conf
+run chown -R odoo /opt/odoo
